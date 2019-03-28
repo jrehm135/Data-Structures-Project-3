544470304153bb985e305a5aff47796d217650b0
Josh Rehm <jrehm135@vt.edu>
2019-03-28 19:03:57 -0400

    Added a record file

---
+++ Data_Block.java
@@ -0,0 +1,10 @@
+
+public class Data_Block {
+    private long RecordID;
+    private double value;
+    
+    public Data_Block(long RecordID, double value) {
+        this.RecordID = RecordID;
+        this.value = value;
+    }
+}
